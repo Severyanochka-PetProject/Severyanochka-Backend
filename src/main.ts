be29c8e7c13f5256b6d2e62aa92f2883fff9561c
TankistPro <nakol.nayk@mail.ru>
2022-01-29 22:50:39 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+
   const config = new DocumentBuilder()
     .setTitle('Severyanochka API')
     .setDescription('Документация по Severyanochka REST API')
