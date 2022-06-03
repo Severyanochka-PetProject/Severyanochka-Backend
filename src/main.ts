7f2d0164bbef8e811760ce94597b59ae17a0e1ce
TankistPro <nakol.nayk@mail.ru>
2022-06-04 01:35:06 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://tankistpro-food.ru', 'http://www.tankistpro-food.ru'],
+    origin: [
+      'http://localhost:3000',
+      'https://tankistpro-food.ru',
+      'https://www.tankistpro-food.ru',
+    ],
     credentials: true,
   });
 
