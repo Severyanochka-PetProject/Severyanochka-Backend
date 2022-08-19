da0915f6497af7d74ea4fe209d1c602bb5945217
TankistPro <nakol.nayk@mail.ru>
2022-08-19 23:56:22 +0300

    f

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,12 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { UserEntity } from '../users/entity/user.entity';
 
 const dbConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'tank',
-  password: 'Qwerty',
-  database: 'severyanochka',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: String(process.env.DB_PASSWORD),
+  database: process.env.DB_DATABASE,
   entities: [UserEntity],
   synchronize: true,
 };
